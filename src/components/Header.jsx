f821113de92a8a9b7d63cd792b454966bac7951f
Lautaro Feruglio <94877419+skinnieraxis1@users.noreply.github.com>
2023-09-06 14:51:06 -0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import './header.css'
+import hotelFondo from '../assets/hotelFondo.jpg'
+import porhotelLogo from '../assets/porHotelLogo.png'
+
+function Header() { 
+
+  return (
+    <>
+      <div className='header'>
+        <div className='zonaImgFondo'>
+            <img className='imgFondo' src={hotelFondo} alt="" />
+        </div>
+        <div className='logoCentral'>
+          <img className='logo' src={porhotelLogo} alt="" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
